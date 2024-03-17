81e23088c54c7b8e8c255078b2953545263a4b56
ilyapasyuk <pasyuk.com@gmail.com>
2024-03-17 20:09:48 +0200

    added build for `Wizard` lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import * as path from 'node:path'
 
 export default defineConfig({
   base: '',
@@ -7,5 +8,21 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+  },
+  build: {
+    minify: true,
+    lib: {
+      entry: path.resolve('./src/Wizard/index.tsx'),
+      name: 'react-onboarding',
+      fileName: (format) => `wizard.${format}.min.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
   },
 })
